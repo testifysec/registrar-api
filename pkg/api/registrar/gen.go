8fc1c214b4ccb08a390b910634cf3a40a1c6e98b
cole kennedy <colek42@gmail.com>
2022-08-04 14:27:59 -0500

    fix prtogen

--- pkg/api/registrar/gen.go
+++ pkg/api/registrar/gen.go
@@ -11,4 +11,4 @@ import (
 //$ curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
 // cd ../  && git clone git@github.com:googleapis/googleapis.git
 
-//go:generate sh -c "protoc -I . --proto_path=.:$GOPATH/src --grpc-gateway_out . --grpc-gateway_opt logtostderr=true --openapiv2_out ./openapiv2 --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true --grpc-gateway-ts_out=ts_import_roots=$(pwd),ts_import_root_aliases=base:../../../ts --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative *.proto"
+//go:generate sh -c "protoc -I . --grpc-gateway_out . --grpc-gateway_opt logtostderr=true --openapiv2_out ./openapiv2 --grpc-gateway_opt paths=source_relative --grpc-gateway_opt generate_unbound_methods=true --grpc-gateway-ts_out=ts_import_roots=$(pwd),ts_import_root_aliases=base:../../../ts --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative *.proto"
